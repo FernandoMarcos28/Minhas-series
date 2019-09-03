210c78efca60be53aef9b5696fb20086651e7c8d
FernandoMarcos28 <shonando27@gmail.com>
2019-09-03 16:22:56 -0300

    inserindo um projeto no github

---
+++ src/Sobre.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+//import Plot from 'react-plotly.js';
+
+class Sobre extends Component{
+    render(){
+        return <h1>Sobre</h1>
+    };
+};
+export default Sobre;
